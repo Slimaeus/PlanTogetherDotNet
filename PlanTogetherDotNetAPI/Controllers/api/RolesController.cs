4d60bcd7908acfe0fbc6abaaa6742571d7e96470
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-03-15 21:54:48 +0700

    feat: add get roles action

--- PlanTogetherDotNetAPI/Controllers/api/RolesController.cs
+++ PlanTogetherDotNetAPI/Controllers/api/RolesController.cs
@@ -20,25 +20,53 @@ namespace PlanTogetherDotNetAPI.Controllers.api
             _userManager = userManager;
             _roleManager = roleManager;
         }
-        [Authorize(Roles = "Admin")]
-        [Route("add-amdmin")]
-        public async Task<IHttpActionResult> GetAddAdmin(string name)
+        public IQueryable<IdentityRole> GetRoles()
+            => _roleManager.Roles;
+        public async Task<IHttpActionResult> PostRole(string name)
         {
-            var user = await _userManager.FindByNameAsync(name);
-            if (user == null) return BadRequest();
-            var adminRole = await _roleManager.FindByNameAsync("Admin");
-            if (adminRole == null) return BadRequest();
-            await _userManager.AddToRoleAsync(user.Id, adminRole.Name);
-            return Ok();
+            var isExists = await _roleManager.RoleExistsAsync(name);
+            if (isExists)
+            {
+                ModelState.AddModelError("role", "Role already existed");
+                return BadRequest(ModelState);
+            }
+            var role = new IdentityRole { Name = name };
+            var result = await _roleManager.CreateAsync(role);
+            if (result.Succeeded) return StatusCode(System.Net.HttpStatusCode.NoContent);
+            return InternalServerError();
+        }
+        public async Task<IHttpActionResult> DeleteRole(string name)
+        {
+            var role = await _roleManager.FindByIdAsync(name);
+            if (role == null)
+            {
+                ModelState.AddModelError("role", "Role did not exist");
+                return BadRequest(ModelState);
+            }
+            var result = await _roleManager.DeleteAsync(role);
+            if (result.Succeeded) return StatusCode(System.Net.HttpStatusCode.NoContent);
+            return InternalServerError();
+        }
+        [Authorize(Roles = "Admin")]
+        [Route("add-role/{username}/{role}")]
+        public async Task<IHttpActionResult> GetAddAdmin(string userName, string roleName)
+        {
+            var user = await _userManager.FindByNameAsync(userName);
+            if (user == null) return NotFound();
+            var role = await _roleManager.FindByNameAsync(roleName);
+            if (role == null) return NotFound();
+            await _userManager.AddToRoleAsync(user.Id, role.Name);
+            return StatusCode(System.Net.HttpStatusCode.NoContent);
         }
         [Authorize(Roles = "Admin")]
         [Route("admin-test")]
-        public async Task<IHttpActionResult> GetAdminTest()
+        public IHttpActionResult GetAdminTest()
         {
             return Ok("You have Admin role o_O");
         }
         [Authorize]
-        public async Task<IHttpActionResult> GetRoles()
+        [Route("get-current-user-roles")]
+        public IHttpActionResult GetUserRoles()
         {
             var identity = (ClaimsIdentity)User.Identity;
             var roles = identity.FindAll(ClaimTypes.Role).Select(c => c.Value);
